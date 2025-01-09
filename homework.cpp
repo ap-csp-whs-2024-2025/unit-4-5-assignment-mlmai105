d88f7d8afe60120f5c77bfc103d4280f4c6b4b66
mlmai105 <mollyle43@gmail.com>
2025-01-09 13:46:37 -0800

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -7,7 +7,22 @@ void displayList(std::vector<T> const &v);
 
 int main()
 {
-    // You can follow along with any code in class here
+    // You can follow along with any code in class here'
+
+    // int x = 5;
+    // double y = 3.14;
+    // std::string name = "Mr.Vu";
+    
+    // std::vector<int> jennys = {8, 6, 7, 5, 3, 0, 9};
+    // //A list of ints named jennys with 7 elements in it
+    // std::vector<double> thy = {3.14, 2.5};
+    // std::<std::string> kaz = {"hello", "darkness", "my", "old", "friend"}; 
+
+    std::vector<std::int> ints = (1, 2, 3, 4, 5);
+    std::vector<std::double> doubles = (1.2, 1.3, 1.4, 1.5);
+    std::vector<std::string> words =("why", "no", "go", "so", "go");
+    displayList(int, 0)
+
 
     
     return 0;
